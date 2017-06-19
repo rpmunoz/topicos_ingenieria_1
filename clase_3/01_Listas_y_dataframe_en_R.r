7d9dd346f76e8041733a04b1a18ba10429987fba
Roberto Munoz <rmunoz@uc.cl>
2017-06-19 17:59:25 -0400

    Added notebooks for clase 3

---
+++ clase_3/01_Listas_y_dataframe_en_R.r
@@ -0,0 +1,109 @@
+
+Sys.setlocale("LC_ALL", 'en_US.UTF-8')
+
+region = c("Arica y Parinacota","Tarapacá","Antofagasta","Atacama","Coquimbo","Valparaíso","Metropolitana de Santiago","Libertador General Bernardo O'Higgins","Maule","Biobío","La Araucanía","Los Ríos","Los Lagos","Aisén del General Carlos Ibáñez del Campo","Magallanes y de la Antártica Chilena")
+poblacion = c(308271,205566,551627,292054,714856,1859312,7150480,903248,1073635,2025995,933537,380618,835829,106893,158828)
+superficie = c(17446.20,41632.60,126048.80,74806.30,40967.80,12646.28,15547.00,16583.30,30340.30,37068.70,31842.30,18577.60,48583.30,107449.40,1392783.70)
+
+class(region)
+class(poblacion)
+class(superficie)
+
+chile = matrix(c(poblacion,superficie), ncol=2, byrow=FALSE)
+chile
+
+class(chile)
+
+colnames(chile)=c("poblacion","superficie")
+rownames(chile)=region
+chile
+
+chile[order(chile[,"poblacion"],decreasing=TRUE),]
+
+chile[order(chile[,"superficie"],decreasing=FALSE),]
+
+chile[,'poblacion']/chile[,'superficie']
+
+densidad = poblacion/superficie
+densidad
+
+chile <- cbind(chile, densidad)
+chile
+
+codigo_region <- c("XV","I","II","III","IV","V","XIII","VI","VII","VIII","IX","XIV","X","XI","XII")
+class(codigo_region)
+
+chile <- cbind(chile, codigo_region)
+chile
+
+class(chile)
+
+chile[,'poblacion']/chile[,'superficie']
+
+class(chile[,'poblacion'])
+class(chile[,'superficie'])
+
+region <- c("Arica y Parinacota","Tarapacá","Antofagasta")
+poblacion <- c(308271,205566,551627)
+superficie <- c(17446.20,41632.60,126048.80)
+codigo_region <- c("XV","I","II")
+
+# Podemos definir los labels de los elements usando names
+chile = list(region, poblacion, superficie, codigo_region)
+names(chile) = c("region","poblacion","superficie","codigo_region")
+chile
+
+# O directamente al momento de definir la lista
+chile = list(region=region, poblacion=poblacion, superficie=superficie, codigo_region=codigo_region)
+chile
+
+chile["region"]
+
+chile[c("region","poblacion")]
+
+chile["poblacion"]/chile["superficie"]
+
+chile[["poblacion"]]/chile[["superficie"]]
+
+region <- c("Arica y Parinacota","Tarapacá","Antofagasta")
+poblacion <- c(308271,205566,551627)
+superficie <- c(17446.20,41632.60,126048.80)
+codigo_region <- c("XV","I","II")
+
+chile <- data.frame(poblacion, superficie, codigo_region)
+colnames(chile) <- c("poblacion","superficie","codigo_region")
+rownames(chile) <- region
+chile
+
+chile["poblacion"]
+
+chile["densidad"] <- chile["poblacion"]/chile["superficie"]
+chile
+
+region = c("Arica y Parinacota","Tarapacá","Antofagasta","Atacama","Coquimbo","Valparaíso","Metropolitana de Santiago","Libertador General Bernardo O'Higgins","Maule","Biobío","La Araucanía","Los Ríos","Los Lagos","Aisén del General Carlos Ibáñez del Campo","Magallanes y de la Antártica Chilena")
+poblacion = c(308271,205566,551627,292054,714856,1859312,7150480,903248,1073635,2025995,933537,380618,835829,106893,158828)
+superficie = c(17446.20,41632.60,126048.80,74806.30,40967.80,12646.28,15547.00,16583.30,30340.30,37068.70,31842.30,18577.60,48583.30,107449.40,1392783.70)
+codigo_region <- c("XV","I","II","III","IV","V","XIII","VI","VII","VIII","IX","XIV","X","XI","XII")
+
+chile <- data.frame(poblacion, superficie, codigo_region)
+colnames(chile) <- c("poblacion","superficie","codigo_region")
+rownames(chile) <- region
+chile
+
+chile[order(poblacion),]
+
+chile[order(-poblacion),]
+
+colMeans(chile["poblacion"])
+
+colMeans(chile[1:2])
+
+apply(chile["poblacion"], 2, mean)
+
+apply(chile["poblacion"], 2, sd)
+
+apply(chile["poblacion"], 2, sum)
+
+summary(chile)
+
+
---
+++ clase_3/03_Visualizaciones_en_R.r
@@ -0,0 +1,87 @@
+
+Sys.setlocale("LC_ALL", 'en_US.UTF-8')
+
+library(dplyr)
+library(ggplot2)
+
+options(repr.plot.width = 6, repr.plot.height = 4.5)
+
+file="data/chile_demographic.csv"
+chile = read.csv(file)
+
+head(chile)
+
+ggplot(chile, aes(x=RegionID, y=Poblacion)) +
+    geom_point(shape=1)      # Use hollow circles
+
+chile %>%
+group_by(RegionID, Region) %>%
+summarise(n_comuna=n(), Poblacion=sum(Poblacion), Superficie=sum(Superficie)) -> chile_region
+
+chile_region
+
+ggplot(chile_region, aes(x=RegionID, y=Poblacion)) +
+    geom_point(shape=16, colour = "red", fill="red", size = 3)
+
+ggplot(chile_region, aes(x=RegionID, y=Poblacion)) +
+    geom_point(shape=16, colour = "red", fill="red", size = 3) +
+    geom_smooth(method='lm', se=FALSE)
+
+ggplot(chile_region, aes(x=RegionID, y=Poblacion)) +
+    geom_point(shape=16, colour = "red", fill="red", size = 3) +
+    geom_smooth(method='lm', se=FALSE, formula= y ~ poly(x, 2))
+
+ggplot(chile, aes(Poblacion)) +
+    geom_histogram(bins=30)
+
+qplot(chile$Poblacion, geom="histogram", bins=30) 
+
+ggplot(chile, aes(Poblacion)) +
+    geom_histogram(bins=20)
+
+ggplot(chile, aes(Poblacion)) +
+    geom_histogram(bins=30, fill=I("blue")) +
+    ggtitle("Disitribución de Población en Chile") + 
+    xlab("Nº habitantes") +
+    ylab("Frecuencia absoluta")
+
+
+qplot(chile$Poblacion, geom="histogram", bins=30,
+      main = "Disitribución de Población en Chile", 
+      xlab = "Nº habitantes",
+      ylab = "Frecuencia absoluta",
+      fill=I("blue"))
+
+ggsave("figures/histograma_poblacion_chile.pdf", width = 16, height = 12, units = "cm")
+
+ggplot(chile_region, aes(x=Region, y=Poblacion)) +
+    geom_bar(stat="identity")
+
+ggplot(chile_region, aes(x=Region, y=Poblacion)) +
+    geom_bar(stat="identity") +
+    theme(axis.text.x=element_text(angle=90,hjust=1,vjust=0.5))
+
+chile_region$Region <- factor(chile_region$Region, levels = chile_region$Region)
+
+ggplot(chile_region, aes(x=Region, y=Poblacion)) +
+    geom_bar(stat="identity") +
+    theme(axis.text.x=element_text(angle=90,hjust=1,vjust=0.5))
+
+ggplot(chile_region, aes(x=Region, y=Poblacion)) +
+    geom_bar(stat="identity", fill=I("blue")) +
+    theme(axis.text.x=element_text(angle=90,hjust=1,vjust=0.5))
+
+ggsave("figures/grafico_barras_poblacion_chile.pdf", width = 16, height = 12, units = "cm")
+
+ggplot(chile, aes(x=Poblacion, y=IDH_2005)) +
+    geom_point(shape=16, colour = "red", fill="red", size = 3) +
+    geom_smooth(method='lm', se=FALSE)
+
+ggplot(chile, aes(x=Poblacion, y=IDH_2005, group=Region, shape=Region, color=Region)) +
+    geom_point(size = 3) + scale_shape_manual(values=seq(0,15)) +
+    theme(legend.position = c(0.8, 0.3), legend.text=element_text(size=5)) +
+    guides(shape=guide_legend(override.aes=list(size=2)))
+
+
+
+
