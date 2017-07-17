13ea8acfb7e06d0022cef9299c98b034f8fc83cd
Roberto Munoz <rmunoz@uc.cl>
2017-07-17 13:49:35 -0400

    Modified INE missing values

--- clase_5/01 - Analisis de datos atipicos y perdidos.R
+++ clase_5/01 - Analisis de datos atipicos y perdidos.R
@@ -8,6 +8,11 @@ Sys.setlocale("LC_ALL", 'Spanish_Chile.1252')
 library(ggplot2)
 library(dplyr)
 
+#Instalamos la libreria VIM: Visualization and Imputation of Missing Values
+install.packages('VIM')
+install.packages('VIMGUI')
+library(VIM)
+
 # Leer archivo desde la web
 url="https://raw.githubusercontent.com/rpmunoz/topicos_ingenieria_1/master/clase_5/data/INE_situacion_fuerza_de_trabajo_missing.csv"
 trabajo=read.csv(url, fileEncoding="UTF-8")
@@ -26,15 +31,42 @@ trabajo$Fecha
 class(trabajo$Fecha)
 
 # Primero notamos que las reconoce como etiquetas y que no las cosidera commo fechas
-trabajo$Fecha <- as.character(trabajo$Fecha)
+trabajo$Fecha <- as.Date(trabajo$Fecha, "%Y-%m-%d")
 class(trabajo$Fecha)
 
-trabajo$Fecha <- as.Date(trabajo$Fecha, "%y-%m")
+# Volvemos a imprimir un resumen del dataframe
+summary(trabajo)
 
-ind <- which( trabajo$Fecha == '' )
+# Existen múltiples maneras de identificar y corregir valores perdidos
+
+# 1. Usar funciones simples para determinar el procentaje de valores perdidos en cada columna
+
+# Calular porcentaje de datos perdidos
+pMiss <- function(x){sum(is.na(x))/length(x)*100}
+apply(trabajo,2,pMiss)
+
+# Buscamos las Fechas que contienen valores NA, los cuales corresponden a valores perdidos
+ind <- which( is.na(trabajo$Fecha) )
 ind
 trabajo[ind,'Fecha']
 
+
+library(VIM)
+aggr_plot <- aggr(data, col=c('navyblue','red'), numbers=TRUE, sortVars=TRUE,
+                  labels=names(data), cex.axis=.7, gap=3, ylab=c("Histogram of missing data","Pattern"))
+
+
+
+
+# Existen múltiples maneras de reemplazar datos perdidos. Aquí se presentan algunas
+
+# 1. Reemplazo manual
+trabajo[4,'Fecha'] <- "2010-04-01"
+trabajo[33,'Fecha'] <- "2012-09-01"
+trabajo[50,'Fecha'] <- "2014-02-01"
+trabajo[67,'Fecha'] <- "2015-07-01"
+
+# 2. 
 
 trabajo[ind,'Fecha']
 
@@ -45,13 +77,12 @@ trabajo[67,'Fecha'] <- "2015-07"
 
 levels(trabajo$Fecha)[33] <- "2012-09"
 
+trabajo$Fecha <- as.character(trabajo$Fecha)
+
 trabajo$Año <- factor(trabajo$Año)
 trabajo$Mes <- factor(trabajo$Mes, levels = unique(trabajo$Mes))
 
 
-# Calular porcentaje de datos perdidos
-pMiss <- function(x){sum(is.na(x))/length(x)*100}
-apply(trabajo,2,pMiss)
 
 ggplot(trabajo, aes(x=Año, y=Fuerza.de.trabajo, color=Año)) +
   geom_boxplot()
