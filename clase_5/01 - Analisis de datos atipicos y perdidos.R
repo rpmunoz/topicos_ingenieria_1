42b3d7974ecd890c696d180f393bf88c49f0d2e1
Roberto Munoz <rmunoz@uc.cl>
2017-07-17 13:13:26 -0400

    Updated INE missing csv

--- clase_5/01 - Analisis de datos atipicos y perdidos.R
+++ clase_5/01 - Analisis de datos atipicos y perdidos.R
@@ -8,23 +8,35 @@ Sys.setlocale("LC_ALL", 'Spanish_Chile.1252')
 library(ggplot2)
 library(dplyr)
 
+# Leer archivo desde la web
 url="https://raw.githubusercontent.com/rpmunoz/topicos_ingenieria_1/master/clase_5/data/INE_situacion_fuerza_de_trabajo_missing.csv"
 trabajo=read.csv(url, fileEncoding="UTF-8")
 
+# Encabezado del dataframe
 head(trabajo)
 
+# Resumen y visualizar datos usando una tabla
 summary(trabajo)
 View(trabajo)
 
+# Revisar columna Fecha y notar que aparecen celdas con conteido vacío
+# Notar la frase "85 Levels" al final. Significa que R considero esta columna como varible cualitativa
+# y creó etiquetas
 trabajo$Fecha
-trabajo$Fuerza.de.trabajo
-trabajo$Desocupados.total
+class(trabajo$Fecha)
 
+# Primero notamos que las reconoce como etiquetas y que no las cosidera commo fechas
+trabajo$Fecha <- as.character(trabajo$Fecha)
+class(trabajo$Fecha)
 
+trabajo$Fecha <- as.Date(trabajo$Fecha, "%y-%m")
 
 ind <- which( trabajo$Fecha == '' )
 ind
-levels(trabajo[50,'Fecha'])
+trabajo[ind,'Fecha']
+
+
+trabajo[ind,'Fecha']
 
 trabajo$Type <- as.character(fixed$Type)
 trabajo[33,'Fecha'] <- "2012-09"
@@ -35,6 +47,11 @@ levels(trabajo$Fecha)[33] <- "2012-09"
 
 trabajo$Año <- factor(trabajo$Año)
 trabajo$Mes <- factor(trabajo$Mes, levels = unique(trabajo$Mes))
+
+
+# Calular porcentaje de datos perdidos
+pMiss <- function(x){sum(is.na(x))/length(x)*100}
+apply(trabajo,2,pMiss)
 
 ggplot(trabajo, aes(x=Año, y=Fuerza.de.trabajo, color=Año)) +
   geom_boxplot()
