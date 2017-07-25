af4569cfaee57583c84c4c76c2e5374f9c790fd6
Roberto Munoz <rmunoz@uc.cl>
2017-07-24 20:54:20 -0400

    Updated enconding to Windows

--- clase_6/01 - Taller datos atipicos.R
+++ clase_6/01 - Taller datos atipicos.R
@@ -22,47 +22,47 @@ url <- "https://raw.githubusercontent.com/rpmunoz/topicos_ingenieria_1/master/cl
 trabajo <- read.csv(url, fileEncoding="UTF-8")
 View(trabajo)
 
-trabajo$AÃ±o <- factor(trabajo$AÃ±o)
+trabajo$Año <- factor(trabajo$Año)
 trabajo$Mes <- factor(trabajo$Mes, levels = unique(trabajo$Mes))
 
-ggplot(trabajo, aes(x=AÃ±o, y=Fuerza.de.trabajo, color=AÃ±o)) +
+ggplot(trabajo, aes(x=Año, y=Fuerza.de.trabajo, color=Año)) +
   geom_boxplot()
 
-ggplot(trabajo, aes(x=Mes, y=Fuerza.de.trabajo, group=AÃ±o, color=AÃ±o)) +
+ggplot(trabajo, aes(x=Mes, y=Fuerza.de.trabajo, group=Año, color=Año)) +
   geom_line() +
   theme(axis.text.x = element_text(angle=90, hjust=1))
 
-ggplot(trabajo, aes(x=Mes, y=Desocupados.total, group=AÃ±o, color=AÃ±o)) +
+ggplot(trabajo, aes(x=Mes, y=Desocupados.total, group=Año, color=Año)) +
   geom_line() +
   theme(axis.text.x = element_text(angle=90, hjust=1))
 
 # Actividad 1
 # -----------
 
-# 1. Analice toda las columnas del dataframe trabajo. Indique el nÃºmero de valores perdidos y el porcentaje
+# 1. Analice toda las columnas del dataframe trabajo. Indique el número de valores perdidos y el porcentaje
 # que representa de cada columna. Identifique los indices e imprima los indices de los valores perdidos de cada columna.
 
-# 2. Corrija los valos perdidos de las columnas Fecha, AÃ±o y Mes de manera manual.
+# 2. Corrija los valos perdidos de las columnas Fecha, Año y Mes de manera manual.
 
 # 3. Analice las columnas Fuerza.de.trabajo y Desocupados.total y cree un modelo lineal de los datos
 # usando la columna Fecha en el eje X. Puede usar Polinomios o Splines. Grafique sus resultados
 
-# 4. Inditifique los valores atÃ­picos de las columnas Fuerza.de.trabajo y Desocupados.total.
+# 4. Inditifique los valores atípicos de las columnas Fuerza.de.trabajo y Desocupados.total.
 # Reemplace estos valores por NA y vuelva a construir los modelos
 
 # 5. Use los modelos anteriormente construidos y complete todos los valores perdidos y outliers
-# del dataframe trabajo usando la funciÃ³n predict de R
+# del dataframe trabajo usando la función predict de R
 
 # Actividad 2
 # -----------
 
 # 1. Una vez reparadao el dataframe trabajo, cree una nueva columna llamada Tasa.desempleo que corresponda
-# a la divisiÃ³n entre las columnas Desocupados.total y Fuerza.de.trabajo
+# a la división entre las columnas Desocupados.total y Fuerza.de.trabajo
 
-# 2. Cree un modelo que permita predecir la columna Tasa.desempleo como funciÃ³n de la Fecha. Use este modelo
+# 2. Cree un modelo que permita predecir la columna Tasa.desempleo como función de la Fecha. Use este modelo
 # para predecir la tasa de desempleo para los meses de Marzo y Abril del 2017.
 
-# 3. Use el archivo publicado por el Instituto Nacional de estadÃ­sticas y compare la estimaciÃ³n que usted hizo
-# Marzo y Abril del 2017 con los valores reales. Use las dos siguientes lÃ­neas para recuperar los valores reales.
+# 3. Use el archivo publicado por el Instituto Nacional de estadísticas y compare la estimación que usted hizo
+# Marzo y Abril del 2017 con los valores reales. Use las dos siguientes líneas para recuperar los valores reales.
 url_ine <- "https://raw.githubusercontent.com/rpmunoz/topicos_ingenieria_1/master/prueba_1/data/INE_situacion_fuerza_de_trabajo.csv"
 trabajo_ine <- read.csv(url_ine, fileEncoding="UTF-8")
