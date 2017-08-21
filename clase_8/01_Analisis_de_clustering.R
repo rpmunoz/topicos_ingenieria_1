7de497299ee800be4b6aa366eb2d2121df80d2a7
Roberto Munoz <rmunoz@uc.cl>
2017-08-21 17:09:15 -0300

    Updated data

--- clase_8/01_Analisis_de_clustering.R
+++ clase_8/01_Analisis_de_clustering.R
@@ -7,6 +7,7 @@ Sys.setlocale("LC_ALL", 'Spanish_Chile.1252')
 #Cargamos las librerias ggplot2 y dplyr
 library(ggplot2)
 library(dplyr)
+library(cluster)
 
 # Leer archivo desde la web
 url <- "https://raw.githubusercontent.com/rpmunoz/topicos_ingenieria_1/master/clase_8/data/vinos_analisis_quimico.csv"
@@ -18,12 +19,37 @@ View(vinos)
 vinos.stand <- scale(vinos)
 
 # La función kmeans permite hacer análisis de agrupaciones o clustering de datos.
-# El parámetro de input corresponde al número de grupos
+# El parámetro de input, k, corresponde al número de grupos
 # Guardamos el resultado en la variable k.means.fit
 k.means.fit <- kmeans(vinos.stand, 2)
 
 # Podemos ver los parámetros que contiene la variable k.means.fit
 attributes(k.means.fit)
 
-# El parámetro cluster corresponde al Id único del cluster que se le asigna a cada elemento de la tabla
+# El atributo cluster corresponde al Id único del cluster que se le asigna a cada elemento de la tabla
 k.means.fit$cluster
+
+# El atributo centers corresponde al valor central de cada uno de los grupos
+k.means.fit$centers
+
+# También es posible ver el número de elementos en cada grupo
+k.means.fit$size
+
+# Una pregunta interesante es saber cuál es el valor óptimo para el parámetro k
+# Una regla es escoger un número de clusters de tal manera que al agregar otro cluster los resultados del modelo no mejoran
+# Uno puede graficar el porcentaje de la varianza que es explicado por el número de clusters vs el número de clusters
+# El primer cluster aportará gran cantidad de información (alta varianza), pero en algún punto la ganancia será marginal
+
+wssplot <- function(data, nc=15, seed=1234){
+  wss <- (nrow(data)-1)*sum(apply(data,2,var))
+  for (i in 2:nc){
+    set.seed(seed)
+    wss[i] <- sum(kmeans(data, centers=i)$withinss)}
+  plot(1:nc, wss, type="b", xlab="Número de clusters",
+       ylab="Suma desviaciones cuadráticas dentro de grupos")}
+
+wssplot(vinos.stand, nc=6) 
+
+clusplot(vinos.stand, k.means.fit$cluster, main='2D representation of the Cluster solution',
+         color=TRUE, shade=TRUE,
+         labels=2, lines=0)
--- clase_8/extra/preparacion_datos.R
+++ clase_8/extra/preparacion_datos.R
@@ -4,10 +4,15 @@ data(wine, package='rattle')
 head(wine)
 wine <- rename(wine, Tipo=Type, Alcohol=Alcohol, Acido.Malico=Malic, Cenizas=Ash, Alcalinidad=Alcalinity, Magnesio=Magnesium, Fenoles=Phenols, Flavonoides=Flavanoids, NoFlavonoides=Nonflavanoids, Proantocianidinas=Proanthocyanins, Tono=Hue, Dilucion=Dilution, Prolina=Proline)
 
+wine <- wine[sample(nrow(wine)),]
+
+wine %>%
+  select(Tipo) -> wine_type
+head(wine_type)
+
 wine %>%
   select(-Tipo) -> wine
-
-wine <- wine[sample(nrow(wine)),]
 head(wine)
 
-write.csv(wine, file="~/Downloads/vinos_analisis_quimico.csv", fileEncoding="UTF-8", row.names=FALSE)
+write.csv(wine, file="../data/vinos_analisis_quimico.csv", fileEncoding="UTF-8", row.names=FALSE)
+write.csv(wine_type, file="../data/vinos_analisis_quimico_tipos.csv", fileEncoding="UTF-8", row.names=FALSE)
